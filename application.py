8826deca2e83f26ffe4caa4b0dbedfe7ff5b5bfb
franckalain <k.franckalain@gmail.com>
2019-10-01 11:51:12 +0000

    Add precompiled assets for Heroku

--- application.py
+++ application.py
@@ -187,7 +187,8 @@ def upload():
 
 if __name__ == '__main__':
     # app.run(port=5002, debug=True)
+    application.run()
 
     # Serve the app with gevent
-    http_server = WSGIServer(('0.0.0.0', 5000), application)
-    http_server.serve_forever()
+    #http_server = WSGIServer(('0.0.0.0', 5000), application)
+    #http_server.serve_forever()
