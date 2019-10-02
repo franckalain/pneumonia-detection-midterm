ed05a88d8bc1aa3af9e286b7d05d4b086e7f4db0
franckalain <k.franckalain@gmail.com>
2019-10-02 09:39:55 +0000

    Add existing files

--- app.py
+++ app.py
@@ -186,9 +186,9 @@ def upload():
 
 if __name__ == '__main__':
     # app.run(port=5002, debug=True)
-    app.run(port=5002, debug=True)
+    #app.run(port=5002, debug=True)
 
     # Serve the app with gevent
     #
-    #http_server = WSGIServer(('0.0.0.0', 5000), application)
-    #http_server.serve_forever()
+    http_server = WSGIServer(('0.0.0.0', 5000), application)
+    http_server.serve_forever()
